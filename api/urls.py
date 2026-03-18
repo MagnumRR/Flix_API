286aa0abe8127308b6978947f89188307ccf6f37
MAGNUM RIBEIRO RODRIGUES DOS SANTOS <39248308+MagnumRR@users.noreply.github.com>
2026-03-17 21:20:50 -0300

    App - Api(endpoints)

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDesctroy
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
+
+router = DefaultRouter()
+router.register(r'genres', GenreCreateListView, GenreRetriveUpdateDestroyView)
+router.register(r'actors', ActorCreateListView, ActorRetrieveUpdateDesctroy)
+router.register(r'movies', MovieCreateListView, MovieRetrieveUpdateDestroyView)
+router.register(r'reviews', ReviewCreateListView, ReviewRetrieveUpdateDestroyView)
+
+urlpatterns = router.urls
